756625fdb82fdb944f121880d3092b85dc638af1
Coelacanthus <liuhongwu2003@outlook.com>
2020-06-07 14:41:10 +0800

    style: add availible time

--- ubuntu14_install_helper.sh
+++ ubuntu14_install_helper.sh
@@ -5,6 +5,7 @@ set -e
 # 打印执行的命令
 set -v
 
+# Since Qt 5.5
 has_asprintf=(  'detaildialog.cpp'
                 'exportutil.cpp'
                 )
@@ -13,7 +14,9 @@ for i in "${has_asprintf[@]}"; do
     sed -i 's/asprintf/sprintf/g' "$i"
 done
 
+# Since Qt 5.4
 sed -i '/<property name="tabBarAutoHide">/,+2d' forms/lemon.ui
+# Since GCC 6.x compiled with option "--enable-default-pie"
 sed -i '/unix:QMAKE_LFLAGS += -no-pie/d' lemon.pro
 
 source ./ubuntu16_install_helper.sh
