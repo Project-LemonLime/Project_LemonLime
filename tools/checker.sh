7ddb8440d6e58e7a2ae7e4e77e12cb9b4acc1927
Coelacanthus <liuhongwu2003@outlook.com>
2020-07-25 20:37:49 +0800

    may be helpful

--- tools/checker.sh
+++ tools/checker.sh
@@ -12,8 +12,8 @@ function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
-version_lt $qt_ver 5.4 && ./qt_5_4.sh ..
-version_lt $qt_ver 5.5 && ./qt_5_5.sh ..
-version_lt $qt_ver 5.7 && ./qt_5_7.sh ..
-version_lt $qt_ver 5.13 && ./qt_5_13.sh ..
-version_lt $qt_ver 5.14 && ./qt_5_14.sh ..
+version_lt "$qt_ver" 5.4 && ./qt_5_4.sh ..
+version_lt "$qt_ver" 5.5 && ./qt_5_5.sh ..
+version_lt "$qt_ver" 5.7 && ./qt_5_7.sh ..
+version_lt "$qt_ver" 5.13 && ./qt_5_13.sh ..
+version_lt "$qt_ver" 5.14 && ./qt_5_14.sh ..
