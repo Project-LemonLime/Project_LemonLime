1d7c7508741e032ac42e8ba4f3a63808d4c3b775
Coelacanthus <liuhongwu2003@outlook.com>
2020-11-25 20:07:35 +0800

    fix: mistake code
    related: #98

--- src/addtestcaseswizard.cpp
+++ src/addtestcaseswizard.cpp
@@ -176,7 +176,7 @@ auto AddTestCasesWizard::getMatchedPart(const QString &str, const QString &patte
 					regExp = QRegularExpression::anchoredPattern(regExp);
 					if (QRegularExpression(regExp).match(str.mid(i, j - i + 1)).hasMatch()) {
 						if (QRegularExpression(QRegularExpression::anchoredPattern(
-						                           getFullRegExp(pattern.mid(pos + 3)) + "$"))
+						                           getFullRegExp(pattern.mid(pos + 3))))
 						        .match(str.mid(j + 1))
 						        .hasMatch()) {
 							result[index] = str.mid(i, j - i + 1);
