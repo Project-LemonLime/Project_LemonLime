09b97b76c72df774a18a698f8b24db8267caba78
Dust1404 <2477259579@qq.com>
2023-07-31 14:05:25 +0800

    add static memory check for macos

--- unix/watcher_macos.cpp
+++ unix/watcher_macos.cpp
@@ -13,7 +13,9 @@
 #include <cstdlib>
 #include <cstring>
 #include <errno.h>
+#include <mach-o/loader.h>
 #include <string>
+#include <sys/fcntl.h>
 #include <sys/resource.h>
 #include <sys/sysctl.h>
 #include <sys/time.h>
@@ -22,6 +24,73 @@
 #include <unistd.h>
 
 int pid;
+
+template <typename header> static ssize_t calculateStaticMemoryUsage(int fd) {
+	ssize_t res = 0;
+
+	header hdr;
+	if (lseek(fd, 0, SEEK_SET) < 0) {
+		return -1;
+	}
+	if (read(fd, &hdr, sizeof(header)) != sizeof(header)) {
+		return -1;
+	}
+	if (hdr.filetype != MH_EXECUTE) {
+		return -1;
+	}
+
+	uint32_t ncmds = hdr.ncmds;
+	while (ncmds--) {
+		struct load_command command;
+		struct segment_command seg;
+		struct segment_command_64 seg64;
+
+		if (read(fd, &command, sizeof(struct load_command)) != sizeof(struct load_command)) {
+			return -1;
+		}
+		if (lseek(fd, -sizeof(struct load_command), SEEK_CUR) < 0) {
+			return -1;
+		}
+
+		switch (command.cmd) {
+			case LC_SEGMENT:
+				if (read(fd, &seg, sizeof(struct segment_command)) != sizeof(struct segment_command)) {
+					return -1;
+				}
+				if (strcmp(seg.segname, SEG_DATA) == 0 || strcmp(seg.segname, SEG_TEXT) == 0) {
+					res += seg.vmsize;
+				}
+				if (sizeof(struct segment_command) != command.cmdsize) {
+					if (sizeof(struct segment_command) > command.cmdsize ||
+					    lseek(fd, command.cmdsize - sizeof(struct segment_command), SEEK_CUR) < 0) {
+						return -1;
+					}
+				}
+				break;
+			case LC_SEGMENT_64:
+				if (read(fd, &seg64, sizeof(struct segment_command_64)) !=
+				    sizeof(struct segment_command_64)) {
+					return -1;
+				}
+				if (strcmp(seg.segname, SEG_DATA) == 0 || strcmp(seg.segname, SEG_TEXT) == 0) {
+					res += seg64.vmsize;
+				}
+				if (sizeof(struct segment_command_64) != command.cmdsize) {
+					if (sizeof(struct segment_command_64) > command.cmdsize ||
+					    lseek(fd, command.cmdsize - sizeof(struct segment_command_64), SEEK_CUR) < 0) {
+						return -1;
+					}
+				}
+				break;
+			default:
+				if (lseek(fd, command.cmdsize, SEEK_CUR) < 0) {
+					return -1;
+				}
+		}
+	}
+
+	return res;
+}
 
 void cleanUp(int dummy) {
 	kill(pid, SIGKILL);
@@ -43,6 +112,36 @@ int main(int argc, char *argv[]) {
 	timeLimit = (timeLimit - 1) / 1000 + 1;
 	sscanf(argv[6], "%d", &memoryLimit);
 	memoryLimit *= 1024 * (isAppleSilicon ? 4 : 1);
+
+	/* check static memory usage */
+	uint32_t magic;
+	ssize_t staticMemoryUsage = 0;
+	int fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		return 1;
+	}
+
+	if (read(fd, &magic, sizeof(uint32_t)) != sizeof(uint32_t)) {
+		close(fd);
+		return 1;
+	}
+	if (magic == MH_MAGIC) {
+		staticMemoryUsage = calculateStaticMemoryUsage<mach_header>(fd);
+	} else if (magic == MH_MAGIC_64) {
+		staticMemoryUsage = calculateStaticMemoryUsage<mach_header_64>(fd);
+	} else {
+		staticMemoryUsage = -1;
+	}
+	close(fd);
+
+	if (staticMemoryUsage == -1) {
+		return 1;
+	}
+	if (staticMemoryUsage > memoryLimit) {
+		printf("0\n%zd\n", staticMemoryUsage);
+		return 0;
+	}
+
 	pid = fork();
 
 	if (pid > 0) {
