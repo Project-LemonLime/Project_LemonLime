f962f8298c9780c92c32c085e856376a7bb79ce2
Alphagocc <alphagocc@163.com>
2026-03-08 03:29:23 +0800

    test: add QTest integration for contest core logic
    Add test1 suite testing the full contest data lifecycle:
    - testReadCdf: loads TestContest1.cdf, verifies title/tasks/contestants
    - testGetCompilerPath: verifies g++ and python are found on PATH
    - testJudgeAllAndCheckOutput: judges all test cases, checks expected scores
    - testWriteCdf: round-trip CDF serialization via writeToJson/readFromJson
    
    CMake changes:
    - enable_testing() + add_subdirectory(tests) in root CMakeLists.txt
    - Qt6::Test and SingleApplication linked for test1 target
    - Test data copied to build dir via file(COPY) at configure time
    - set_tests_properties ENVIRONMENT for Qt DLL path on Windows
    
    (cherry picked from commit 5391f249ce0f3b0c493f24627c22eb4736000fe0)

---
+++ tests/data/TestContest1/source/user1/helloworld.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n){vector<int> a(1);
+for(int i=0;i<n;i++)
+{
+	a[i]=100;
+}}
+int main()
+{
+	int s;
+	cin >> s;
+	switch (s)
+	{
+		case 1: cout << "AC" << endl; break;
+		case 2: cout << "WA" << endl; break;
+		case 3: while(1)new int[1<<24];
+		case 4: while(1); break;
+		case 5: f(100); break;
+		default : break;
+	}
+}
