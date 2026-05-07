56c0f4b1f7e75d64ab1d5a6f42203864d02e80f3
ZnPdCo <poiuytreertyuiop@outlook.com>
2026-05-07 23:51:25 +0800

    feat(exportutil): add "return to top" text and improve code formatting

--- src/component/exportutil/exportutil.cpp
+++ src/component/exportutil/exportutil.cpp
@@ -87,6 +87,7 @@ QJsonObject ExportUtil::buildExportJson(Contest *contest) {
 	i18n["time"] = tr("Time Used");
 	i18n["memory"] = tr("Memory Used");
 	i18n["score"] = tr("Score");
+	i18n["return_to_top"] = tr("Return to top");
 	root["i18n"] = i18n;
 
 	// task_names
@@ -163,8 +164,7 @@ QJsonObject ExportUtil::buildExportJson(Contest *contest) {
 			    taskList[j]->getTaskType() == Task::Interaction ||
 			    taskList[j]->getTaskType() == Task::Communication ||
 			    taskList[j]->getTaskType() == Task::CommunicationExec) {
-				if (contestant->getCheckJudged(j) &&
-				    contestant->getCompileState(j) == CompileSuccessfully) {
+				if (contestant->getCheckJudged(j) && contestant->getCompileState(j) == CompileSuccessfully) {
 					tObj["file"] = contestant->getSourceFile(j);
 				}
 			}
@@ -243,7 +243,7 @@ QJsonObject ExportUtil::buildExportJson(Contest *contest) {
 							dObj["full_score"] = 0;
 						}
 
-						if (!message[jj][k].isEmpty()) {
+						if (! message[jj][k].isEmpty()) {
 							dObj["info"] = message[jj][k];
 						}
 
@@ -268,7 +268,7 @@ QJsonObject ExportUtil::buildExportJson(Contest *contest) {
 void ExportUtil::exportHtml(QWidget *widget, Contest *contest, const QString &fileName) {
 	QFile file(fileName);
 
-	if (!file.open(QFile::WriteOnly)) {
+	if (! file.open(QFile::WriteOnly)) {
 		QMessageBox::warning(widget, tr("LemonLime"),
 		                     tr("Cannot open file %1").arg(QFileInfo(file).fileName()), QMessageBox::Ok);
 		return;
@@ -279,7 +279,7 @@ void ExportUtil::exportHtml(QWidget *widget, Contest *contest, const QString &fi
 	// Read template from Qt resource
 	QFile templateFile(":/export/export_template.html");
 
-	if (!templateFile.open(QFile::ReadOnly | QFile::Text)) {
+	if (! templateFile.open(QFile::ReadOnly | QFile::Text)) {
 		QApplication::restoreOverrideCursor();
 		QMessageBox::warning(widget, tr("LemonLime"), tr("Cannot read export template"), QMessageBox::Ok);
 		return;
@@ -309,7 +309,7 @@ void ExportUtil::exportHtml(QWidget *widget, Contest *contest, const QString &fi
 void ExportUtil::exportCsv(QWidget *widget, Contest *contest, const QString &fileName) {
 	QFile file(fileName);
 
-	if (!file.open(QFile::WriteOnly)) {
+	if (! file.open(QFile::WriteOnly)) {
 		QMessageBox::warning(widget, tr("LemonLime"),
 		                     tr("Cannot open file %1").arg(QFileInfo(file).fileName()), QMessageBox::Ok);
 		return;
@@ -340,12 +340,6 @@ void ExportUtil::exportCsv(QWidget *widget, Contest *contest, const QString &fil
 		} else {
 			rankList.insert(sortList[i].second, i);
 		}
-	}
-
-	QHash<Contestant *, int> loc;
-
-	for (int i = 0; i < contestantList.size(); i++) {
-		loc.insert(contestantList[i], i);
 	}
 
 	out << "\"" << tr("Rank") << "\"" << "," << "\"" << tr("Name") << "\"" << ",";
